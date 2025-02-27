43375c323d676022be0682b7e22d30233e476c60
JOMUMWE <muindijoshua032@gmail.com>
2025-02-27 09:42:09 +0300

    huhuhuhu

--- shazie_2.0/client/src/components/Dash.jsx
+++ shazie_2.0/client/src/components/Dash.jsx
@@ -12,7 +12,7 @@ export default function Dash() {
       </header>
       <main>
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          {/* Your content */}
+         
         </div>
       </main>
     </div>
